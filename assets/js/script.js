61392a28a15e4190ca17b2cc09ea86be09b679b1
Franklin <47985472+rubensfranklin@users.noreply.github.com>
2021-05-21 05:36:36 -0300

    mensagens de erro

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,49 @@
-const $html = document.querySelector('html')
-const $checkbox = document.querySelector('#switch')
+const button = document.getElementById('button')
 
-$checkbox.addEventListener('change',function(){
-    $html.classList.toggle('dark-mode')
+button.addEventListener('click',(event) =>{
+    event.preventDefault()
+
+    const nome = document.getElementById('nome')
+    
+    const cpf = document.getElementById('cpf')
+
+    const numero = document.getElementById('numero')
+
+    const Celular = document.getElementById('celular')
+
+    
+
+    if(nome.value == ''){
+        alert('Preencha o nome');
+    }
+
+    
+    if(cpf.value == ''){
+        alert('Preencha o CPF');
+    }
+
+
+    if(celular.value == ''){
+        alert('Preencha o Numero de celular sem o 0');
+    }
+
+    
+    if(numero.value == ''){
+        alert('Preencha o numero da casa');
+    }
+
+
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
